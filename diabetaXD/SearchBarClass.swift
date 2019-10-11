37cd44fcb7c018bc48ad16f296eab88d95763233
Aluno Mack <alunomack@10-44-48-135.0200.mack.br.iosda.org>
2019-10-11 08:27:48 -0300

    marcelao

---
+++ diabetaXD/SearchBarClass.swift
@@ -0,0 +1,15 @@
+//
+//  SearchBarClass.swift
+//  diabetaXD
+//
+//  Created by Aluno Mack on 10/10/19.
+//  Copyright © 2019 Aluno Mack. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarClass: UISearchBar, UISearchBarDelegate {
+
+    
+
+}
